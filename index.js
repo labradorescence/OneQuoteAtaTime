d8600a9271ed081a74445d6394e0f551b1a1d1ff
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-11-04 23:43:29 -0500

    emoji loader

--- index.js
+++ index.js
@@ -12,6 +12,29 @@
 //             } 
 //         }; 
 
+const loader = document.querySelector('.preload');
+const emoji = loader.querySelector('.emoji');
+
+const emojis = ["🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚", "🕛", "🕜", "🕝", "🕞", "🕟", "🕠", "🕡", "🕢", "🕣", "🕤", "🕥", "🕦", "🕧"];
+
+const interval = 125;
+
+const loadEmojis = (arr) => {
+  setInterval(() => {
+    emoji.innerText = arr[Math.floor(Math.random() * arr.length)];
+    console.log(Math.floor(Math.random() * arr.length))
+  }, interval);
+}
+
+const init = () => {
+  loadEmojis(emojis);
+ 
+}
+
+init();
+
+
+
 
 
 /**********************  DOM Elements  ***********************/
